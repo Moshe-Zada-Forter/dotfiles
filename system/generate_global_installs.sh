132db5d6ed44d3f1635a5c5f4f5f85d86117ef6c
Moshe Zada <zada.moshe@gmail.com>
2021-08-07 16:01:30 +0300

    Update deps

--- system/generate_global_installs.sh
+++ system/generate_global_installs.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo 'Generating python requirements'
-pip freeze > ~/.yadr/system/requirements.txt
-
 echo 'Generating python3 requirements'
-pip3 freeze > ~/.yadr/system/requirements3.txt
+pip freeze > ~/.yadr/system/requirements.txt
 
 echo 'Generating nodejs packages'
 npm list -g --depth=0 --json > ~/.yadr/system/package.json
@@ -12,5 +9,3 @@ npm list -g --depth=0 --json > ~/.yadr/system/package.json
 echo 'Generating brewfile'
 rm Brewfile && brew bundle dump
 
-echo 'Generating Code plugins'
-code --list-extensions > ~/.yadr/vscode/plugins
